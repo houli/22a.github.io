8c01cd69650cf18512daf53e563d9314864f9791
Peter Meehan <meehanp2@tcd.ie>
2015-04-05 22:58:39 +0100

    Mobile friendly, maybe, Updated styles, updated content.

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -14,7 +14,7 @@ angular.module('meehan.controllers', [])
 .controller('DateController', ['$scope', '$filter', function($scope, $filter) {
 	today = new Date();
 	$scope.year = $filter("date")(today,'yyyy') - 1995;
-	if(($filter("date")(today,'M') < 4) || (($filter("date")(today,'M') === 4 )&&( $filter("date")(today,'d') < 13 )))
+	if((parseInt($filter("date")(today,'M')) < 4) || (($filter("date")(today,'M') === "4" )&&(parseInt($filter("date")(today,'d')) < 13 )))
 	{
 		$scope.year = $scope.year - 1;
 	}
